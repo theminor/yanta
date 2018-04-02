bd2a70cba1e2bf3c3992df8749b9a03d900995ad
John <theminor@users.noreply.github.com>
2018-04-02 16:15:30 -0400

    flesh-out service worker

--- sw.js
+++ sw.js
@@ -1,8 +1,26 @@
-let cacheName = 'yanta';
-let filesToCache = [
-  './',
-  './index.html',
-  './node_modules/ace-builds/src/ace.js',
-  './node_modules/ace-builds/src/mode-markdown.js',
-  './theme-yanta.js'
-];
+self.addEventListener('install', e => {
+	e.waitUntil(
+		caches.open('yanta').then(cache => {
+			return cache.addAll([
+			  './',
+			  './index.html',
+			  './yanta.css',
+			  './node_modules/ace-builds/src/ace.js',
+			  './node_modules/ace-builds/src/mode-markdown.js',
+			  './theme-yanta.js'
+			]);
+		});
+	);
+});
+
+self.addEventListener('activate', event => {
+	event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+	event.respondWith(
+		caches.match(event.request, {ignoreSearch:true}).then(response => {
+			return response || fetch(event.request);
+		})
+	);
+});
