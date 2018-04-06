38afff5b6a0bba3e97bd3ed574b6b10bd78e26e9
John <theminor@users.noreply.github.com>
2018-04-06 12:55:24 -0400

    temp debugging

--- sw.js
+++ sw.js
@@ -1,7 +1,15 @@
 self.addEventListener('install', e => {
 	e.waitUntil(
 		caches.open('yanta').then(cache => {
-			return cache.addAll([
+			// return cache.addAll([
+			cache.add('./').catch(console.log('./'));
+			cache.add('./index.html').catch(console.log('./index.html'));
+			cache.add('./index.js').catch(console.log('./index.js'));
+			cache.add('./index.css').catch(console.log('./index.css'));
+			cache.add('./ace/ace.js').catch(console.log('./ace/ace.js'));
+			cache.add('./ace/mode-markdown.js').catch(console.log('./ace/mode-markdown.js'));
+			cache.add('./theme-yanta.js').catch(console.log('./theme-yanta.js'));
+			/*
 				'./',
 				'./index.html',
 				'./index.js',
@@ -9,7 +17,8 @@ self.addEventListener('install', e => {
 				'./ace/ace.js',
 				'./ace/mode-markdown.js',
 				'./theme-yanta.js'
-			]);
+			*/
+			// ]);
 		})
 	);
 });
