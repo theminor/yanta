5d0c8774e240a5113afd4d792b65f408e01bdce1
John <theminor@users.noreply.github.com>
2018-04-06 09:28:09 -0400

    Require Basic Authentication login

--- yanta.js
+++ yanta.js
@@ -7,15 +7,16 @@ let cache = {};
 
 // default settings:
 let srvSettings = {
-	port: 45464,								// Server Port
-	urlBase: '/yanta',							// url base for hosting
-	useHttps: false,							// use https instead of http (requires cert and key)
-	key: '/path/to/key.pem',					// https key
-	cert: '/path/to/cert.pem',					// https certificate
-	storageDir: '/path/to/storage',				// directory to store notes in
-	oauthFile: '/oauth/credentials.json'		// Oauth2 credentials file, for example from https://developers.google.com/identity/sign-in/web/
+	port: 45464,													// Server Port
+	urlBase: '/yanta',												// url base for hosting
+	useHttps: false,												// use https instead of http (requires cert and key)
+	key: '/path/to/key.pem',										// https key
+	cert: '/path/to/cert.pem',										// https certificate
+	storageDir: '/path/to/storage',									// directory to store notes in
+	userName: 'user',												// username for basic authentication
+	password: 'secret'												// password for basic authentication
 };
-try { srvSettings = Object.assign(srvSettings, require('./srvSettings.json')); }
+try { srvSettings = Object.assign(srvSettings, require('./srvSettings.json')); }	// load alternate settings from srvSettings.json, if it exists
 catch(err) { console.log('Error parsing settings file: ' + err + '. Using default settings.'); }
 if (srvSettings.useHttps) {
 	http = require('https'); 
@@ -23,9 +24,8 @@ if (srvSettings.useHttps) {
 	srvSettings.cert = fs.readFileSync(srvSettings.cert);
 } else http = require('http');
 
-// simple server
-const srv = http.createServer((req, res) => {
-	async function sendStatic(file) {
+const srv = http.createServer((req, res) => { 						// create simple server
+	async function sendStatic(file) {								// response function
 		let contentType = 'text/html';
 		if (file.endsWith('.js')) contentType = 'text/javascript';
 		else if (file.endsWith('.css')) contentType = 'text/css';
@@ -47,11 +47,25 @@ const srv = http.createServer((req, res) => {
 		res.writeHead(200, {'Content-Type': contentType });
 		res.end(cache[file], 'utf-8');
 	}
-	let path = req.url.replace(srvSettings.urlBase, '');
-	if (path === '' || path === '/') sendStatic('./index.html');
-	else if (path.endsWith('theme-yanta.js')) sendStatic('./theme-yanta.js');
-	else if (path.startsWith('/ace/')) sendStatic('./node_modules/ace-builds/src/' + path.replace('/ace/', ''));
-	else if (path.startsWith('/icons/') || path.startsWith('/oauth/')) sendStatic('.' + path);
-	else if (path.endsWith('.html') || path.endsWith('.css') || path.endsWith('.json') || path.endsWith('.txt') || (path.endsWith('.js') && !path.endsWith('yanta.js')) ) sendStatic('.' + path);
+	
+	var auth = req.headers['authorization'];
+	if (auth) {
+		let [usr, pswd] = (new Buffer(auth.replace('Basic ', ''), 'base64')).toString('utf8').split(':');		// remove 'Basic ', then convert to base 64, then split "username:password"
+		if (usr === srvSettings.userName && pswd === srvSettings.password) {
+			let path = req.url.replace(srvSettings.urlBase, '');			// basic routing
+			if (path === '' || path === '/') sendStatic('./index.html');
+			else if (path.endsWith('theme-yanta.js')) sendStatic('./theme-yanta.js');
+			else if (path.startsWith('/ace/')) sendStatic('./node_modules/ace-builds/src/' + path.replace('/ace/', ''));
+			else if (path.startsWith('/icons/') || path.startsWith('/oauth/')) sendStatic('.' + path);
+			else if (path.endsWith('.html') || path.endsWith('.css') || (path.endsWith('.json') && !path.endsWith('srvSettings.json')) || path.endsWith('.txt') || (path.endsWith('.js') && !path.endsWith('yanta.js')) ) sendStatic('.' + path);			
+		} else {
+			res.statusCode = 403;
+			return res.end('Login Failed');
+		}
+	} else {														// authentication is needed
+		res.statusCode = 401;
+		res.setHeader('WWW-Authenticate', 'Basic realm="yanta"');
+		return res.end('Login Required');
+	}
 });
 srv.listen(srvSettings.port);
