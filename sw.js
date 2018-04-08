98254473153d5bbde7d929772c4b55d52d8602d6
John <theminor@users.noreply.github.com>
2018-04-08 12:41:15 -0400

    Change sw behavior to check network first

--- sw.js
+++ sw.js
@@ -19,10 +19,24 @@ self.addEventListener('activate', event => {
 	event.waitUntil(self.clients.claim());
 });
 
+/*
 self.addEventListener('fetch', event => {
 	event.respondWith(
 		caches.match(event.request, {ignoreSearch:true}).then(response => {
 			return response || fetch(event.request);
+		})
+	);
+});
+*/
+
+// network, then cache, if timeout
+self.addEventListener('fetch', event => {
+	event.respondWith(
+		fetch(event.request).then((response) => {
+			if (response.ok) { return cache.put(event.request, response); }
+			else { return caches.match(event.request); }
+		}).catch(() => { 
+			return caches.match(event.request);
 		})
 	);
 });
